6c41df93389d3843f6110fd2276cbfe9ef1cc809
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-16 18:12:55 -0300

    configuração do banco de dados e do settings

--- estudos/django/alurareceita/settings.py
+++ estudos/django/alurareceita/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'user_con',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost'
     }
 }
 
--- estudos/django/receitas/models.py
+++ estudos/django/receitas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.models.IntegerField()
+    rendimento = models.TextField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+)
