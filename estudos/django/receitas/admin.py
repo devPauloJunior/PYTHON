a6ce9ee6306fa6e6c0f618bfdd7370271b3100e4
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-19 20:35:31 -0300

    Implementação do ambiente de ADM Django

--- estudos/django/receitas/admin.py
+++ estudos/django/receitas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Receita
 
-# Register your models here.
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+
+admin.site.register(Receita, ListandoReceitas)
--- estudos/django/receitas/models.py
+++ estudos/django/receitas/models.py
@@ -3,19 +3,10 @@ from datetime import datetime
 
 class Receita(models.Model):
     nome_receita = models.CharField(max_length=200)
-    ingredientes = models.models.TextField()
+    ingredientes = models.TextField()
     modo_preparo = models.TextField()
-    tempo_preparo = models.models.IntegerField()
-    rendimento = models.TextField(max_length=100)
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
-
-    class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+    data_criacao = models.DateTimeField(auto_now_add=True)
