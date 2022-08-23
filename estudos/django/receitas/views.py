94bbd0cb3ae74e2aeb7355926ea7ecd68e2f528e
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-23 20:50:57 -0300

    Resultado da busca

--- estudos/django/receitas/views.py
+++ estudos/django/receitas/views.py
@@ -22,4 +22,15 @@ def receita(request, receita_id):
     return render(request, 'receita.html', receita_a_exibir)
 
 def buscar(request):
-    return render(request, 'buscar.html')    
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+    
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas' : lista_receitas
+    }
+
+    return render(request, 'buscar.html', dados)    
