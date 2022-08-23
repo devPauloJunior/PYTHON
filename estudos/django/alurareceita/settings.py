cdce11299a6f0811a56207b402bb78d9bab5c860
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-23 18:04:10 -0300

    Foto para cada receita

--- estudos/django/alurareceita/settings.py
+++ estudos/django/alurareceita/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/meida/'
--- estudos/django/receitas/models.py
+++ estudos/django/receitas/models.py
@@ -12,4 +12,5 @@ class Receita(models.Model):
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
