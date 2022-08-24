3bee5bd94dbbfdc5b65bd3e6b7a7cb35632dbaac
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-23 21:17:57 -0300

    Autorização e usuários

--- estudos/django/receitas/models.py
+++ estudos/django/receitas/models.py
@@ -14,3 +14,5 @@ class Receita(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome_receita    
