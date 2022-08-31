310d7a027e37addfc52368726e5a6f26505b1e54
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-08-30 22:05:10 -0300

    Configurando o Django Admin

--- estudos/django-rest/escola/models.py
+++ estudos/django-rest/escola/models.py
@@ -17,9 +17,9 @@ class Curso(models.Model):
     }
 
     codigo_curso = models.CharField(max_length=10)
-    descriacao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
     nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
 
     def __str__(self):
-        return self.descriacao
+        return self.descricao
     
