455e3680f966c9e6f2672db8c049ab9ef5b27585
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-09-03 18:14:42 -0300

    Validando todos os campos

--- estudos/django-rest2/clientes/serializers.py
+++ estudos/django-rest2/clientes/serializers.py
@@ -8,5 +8,21 @@ class ClienteSerializer(serializers.ModelSerializer):
 
     def validate_cpf(self, cpf):
         if len(cpf) != 11:
-            raise serializer.ValdationError("O cpf deve conter 11 digitos")
+            raise serializers.ValidationError("O cpf deve conter 11 digitos")
         return cpf  
+    
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Não colocar numeros no campo do nome")
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O rg deve conter 9 digitos")
+        return rg
+   
+    def validate_celular(self, celular):
+        if len(celular) != 11:
+            raise serializers.ValidationError("O numero de celular deve conter 11 digitos")
+        return celular
+
