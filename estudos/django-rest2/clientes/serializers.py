f3ae44f37db69eacefb4a497b244d4c7c3debae1
Paulo Junior <105656681+devPauloJunior@users.noreply.github.com>
2022-09-06 12:58:03 -0300

    Conf validator do celular com Expressões regulares

--- estudos/django-rest2/clientes/serializers.py
+++ estudos/django-rest2/clientes/serializers.py
@@ -14,5 +14,5 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':"O RG deve conter 9 digitos"})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular':"O numero de celular deve conter 11 digitos"})
+            raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"})
         return data
--- estudos/django-rest2/clientes/validators.py
+++ estudos/django-rest2/clientes/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_valido(numero_do_cpf):
         return len(numero_do_cpf) == 11
 
@@ -8,4 +10,7 @@ def rg_valido(numero_do_rg):
         return len(numero_do_rg) == 9
 
 def celular_valido(numero_do_celular):
-        return len(numero_do_celular) == 11
+        """ varificar o celular para 11 91234-4321"""
+        modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+        resposta = re.findall(modelo, numero_do_celular)
+        return resposta
