ba6d313aa9aeec34168f9d8956b80e390d0b2c70
Paulo Junior <paulo.junior@avanz.com.br>
2022-10-17 19:21:52 -0300

    Implementando Adivinhação do Alura

---
+++ adivinhacao.py
@@ -0,0 +1,49 @@
+import random
+
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+
+print("""Qual oinvel de dificuldade? 
+    (1) facil
+    (2) medio
+    (3) dificil
+""")
+
+nivel = ("Defina o nivel: ")
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+print("Fim do jogo")
