7b09ad6ec76c7cefad59c0aca76d373f557bc255
Paulo Junior <paulo.junior@avanz.com.br>
2022-11-08 10:59:55 -0300

    implementação do FORCA

--- forca.py
+++ forca.py
@@ -1,10 +1,15 @@
-def jogar():
-    print("*"*30)
-    print("Bem vindo ao jogo da Forca!")
-    print("*"*30)
+import random
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+def jogar():
+
+    mensagem_abertura()    
+
+
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta ]
 
     enforcou = False
     acertou = False
@@ -25,13 +30,32 @@ def jogar():
                 index += 1
         else:
             erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(len(palavra_secreta)-erros))
 
-        enforcou = erros == 6
+        enforcou = erros == len(palavra_secreta)
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
 
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim de Jogo!")
+
+def mensagem_abertura():
+    print("*"*30)
+    print("Bem vindo ao jogo da Forca!")
+    print("*"*30)
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
 
 if (__name__ == "__main__"):
     jogar()
