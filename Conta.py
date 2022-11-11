3a803f82908802d5d1482c89ab0b0a7c287c283e
Paulo Junior <paulo.junior@avanz.com.br>
2022-11-11 19:38:41 -0300

    Colocando os atributos em  privados

--- Conta.py
+++ Conta.py
@@ -2,7 +2,16 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+    
+    def depositar(self, valor):
+        self.saldo += valor
+    
+    def sacar(self, valor):
+        self.saldo -= valor
