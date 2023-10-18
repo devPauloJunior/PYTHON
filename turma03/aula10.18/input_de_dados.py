15859fed6b21a1dd413c355437d1758d82bd2c88
Paulo Junior <npaulo.ce@gmail.com>
2023-10-18 15:46:57 -0300

    Foi adicionado o arquivo input_de_dados.py

---
+++ turma03/aula10.18/input_de_dados.py
@@ -0,0 +1,6 @@
+#  para criar uma interação com o uspario utilizamos a função INPUT
+# 1. todo INPUT deve ter seu valor associado a uma variavel
+
+nome = input('Digite seu nome aqui: ')
+
+print(nome)
