6781dd80e88733c5adbb14a95816fd4708f549b7
Paulo Junior <npaulo.ce@gmail.com>
2023-10-19 15:33:50 -0300

    conteudo sobre tipos adiconado

--- turma03/aula10.19/tipos02.py
+++ turma03/aula10.19/tipos02.py
@@ -1,2 +1,13 @@
 # entender tipos e mudanças de tipos dentro do python
 
+a = int(50)
+b = "50"
+print(type(a))
+print(type(b))
+
+idade = int(input("Qual sua idade? "))
+print(type(idade))
+
+# utilizanção do float: moedas, peso, temperatura, medidas e outros casos parecidos
+peso = float(input("Qual seu peso? "))
+print(type(peso))
