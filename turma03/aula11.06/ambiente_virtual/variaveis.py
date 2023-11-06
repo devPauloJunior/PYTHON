0a4beeccf363f89ddf43a70f7faa8226b435d6fc
Paulo Junior <npaulo.ce@gmail.com>
2023-11-06 16:33:29 -0300

    Aula ambiente virtual 11.06

---
+++ turma03/aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,14 @@
+# variaveis são usadas para salvar algo na memoria do computador
+# o sinal de = é utilizado para atibuição de valores
+var01 = "alguma coisa"
+print(var01)
+var01 = "outra coisa"
+print(var01)
+var01 = 10
+print(var01)
+var02 = "A" + "B" + "C"
+print(var02)
+var02 = "A" * 10
+print(var02)
+var02 = 10 * "Paulo "
+print(var02, var01, sep="/")
