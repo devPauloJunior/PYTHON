9830df22b3182f92b71ec83ddcc9937af623914e
Paulo Junior <npaulo.ce@gmail.com>
2023-11-21 17:29:43 -0300

    Update atv01

--- turma03/aula11.21/atv01.py
+++ turma03/aula11.21/atv01.py
@@ -1,4 +1,3 @@
 # Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares e a quantidade de números impares.
-while True:
-    numeros_interios = int(input('Digite 10 numeros inteiros: '))
+
     
